2f93d6a5eb0921c5eb877cdcdd5ae83122178c81
Ana Paula <ap.almeidapm@gmail.com>
2026-08-06 21:54:31 -0300

    Add project files.

---
+++ PixelBurguer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PixelBurguer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
