ef403ead87520c11d2c47b110fbe7c134499e042
Ana Paula <ap.almeidapm@gmail.com>
2026-08-11 16:45:05 -0300

    Create models Category and Snack

---
+++ PixelBurguer/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace PixelBurguer.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+        public List<Snack> Snacks { get; set; }
+    }
+}
---
+++ PixelBurguer/Models/Snack.cs
@@ -0,0 +1,27 @@
+﻿namespace PixelBurguer.Models
+{
+    public class Snack
+    {
+        public int SnackId { get; set; }
+
+        public string SnackName { get; set; }
+
+        public string SnackDescriptionShort { get; set; }
+
+        public string SnackDescriptionLong { get; set; }
+
+        public decimal SnackPrice { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string ImageThumbnailUrl { get; set; }
+
+        public bool IsSnackFavorite { get; set; }
+
+        public bool IsSnackAvailable { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        }
+}
