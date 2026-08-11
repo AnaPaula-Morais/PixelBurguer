ce511760d7a60f11475b352dcdbf1902c87afd84
Ana Paula <ap.almeidapm@gmail.com>
2026-08-11 20:06:43 -0300

    configurations for using Entity Framework

---
+++ PixelBurguer/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PixelBurguer.Models;
+
+namespace PixelBurguer.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Snack> Snacks { get; set; }
+    }
+}
