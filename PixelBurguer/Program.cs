6da772b31685f3da14e149de7f8308baec62d1e9
Ana Paula <ap.almeidapm@gmail.com>
2026-08-21 17:18:59 -0300

    Configuration Session and HttpContext

--- PixelBurguer/Program.cs
+++ PixelBurguer/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddTransient<ISnackRepository, SnackRepository>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -29,6 +33,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
