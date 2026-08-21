cbf2d7792d222b963064a95b515829146786c7ac
Ana Paula <ap.almeidapm@gmail.com>
2026-08-21 17:54:44 -0300

    Create shopping cart and method AddCart

---
+++ PixelBurguer/Models/ShoppingCart.cs
@@ -0,0 +1,65 @@
+﻿using PixelBurguer.Context;
+
+namespace PixelBurguer.Models
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _context;
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            //definition a session
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //get a service type our context
+            var context = services.GetService<AppDbContext>();
+
+            //get or create cart id
+            string cartId = session.GetString("Cart")?? Guid.NewGuid().ToString();
+
+            //set cart id in session
+            session.SetString("cartId", cartId);
+
+            return new ShoppingCart(context)
+            {
+                ShoppingCartId = cartId,
+            };
+        }
+
+        public void AddCart(Snack snack)
+        {
+            var shoppingCartItem = _context.ShoppingCartIntems.SingleOrDefault(
+                s => s.Snack.SnackId == snack.SnackId &&
+                s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Snack = snack,
+                    Quantity = 1
+                };
+
+                _context.ShoppingCartIntems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Quantity++;
+            }
+
+            _context.SaveChanges();
+        }
+
+
+    }
+}
